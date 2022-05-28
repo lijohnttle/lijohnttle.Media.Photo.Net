146384b0ae74c0425d9bf910dcbba9f70b87a739
Ivan Cherkasov <lijohnttle@outlook.com>
2022-05-28 18:27:45 +0200

    Add demo project

---
+++ src/lijohnttle.Media.Photo.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using lijohnttle.Media.Photo.App.ViewModels.Common;
+
+namespace lijohnttle.Media.Photo.App.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+    }
+}
---
+++ src/lijohnttle.Media.Photo.App/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using lijohnttle.Media.Photo.App.ViewModels;
+using System.Windows;
+
+namespace lijohnttle.Media.Photo.App.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
