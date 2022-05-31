b0b280c75ed9742e03c013db45b02f5a407e96d0
Ivan Cherkasov <lijohnttle@outlook.com>
2022-05-31 19:14:33 +0200

    Refactor

--- src/lijohnttle.Media.Photo.Filters/Median/MedianFilterKernel.cs
+++ src/lijohnttle.Media.Photo.Filters/Median/MedianFilterKernel.cs
@@ -1,49 +1,27 @@
 ﻿using lijohnttle.Media.Photo.Core;
-using System;
+using lijohnttle.Media.Photo.Filters.Tools;
 using System.Collections.Generic;
 
 namespace lijohnttle.Media.Photo.Filters
 {
-    public class MedianFilterKernel
+    public class MedianFilterKernel : SquareKernel
     {
         private readonly Queue<IColor> pixels = new Queue<IColor>();
-        private readonly int radius;
-        private IImage image;
-        private int leftPadding;
-        private int rightPadding;
-        private int topPadding;
-        private int bottomPadding;
 
 
         public MedianFilterKernel(IImage image, int radius, int positionX, int positionY)
+            : base(image, radius, positionX, positionY)
         {
-            this.image = image;
-            this.radius = radius;
-
-            SetPosition(positionX, positionY);
+            MoveTo(positionX, positionY);
         }
 
 
-        public int PositionX { get; private set; }
-
-        public int PositionY { get; private set; }
-
-        public int Count => pixels.Count;
-
-        public int Left => PositionX - leftPadding;
-
-        public int Right => PositionX + rightPadding;
-
-        public int Top => PositionY - topPadding;
-
-        public int Bottom => PositionY + bottomPadding;
-
-        public int Width => leftPadding + rightPadding + 1;
-
-        public int Height => topPadding + bottomPadding + 1;
+        public override int Count => pixels.Count;
 
 
-        public void SetPosition(int positionX, int positionY)
+        public override IEnumerable<IColor> GetPixels() => pixels;
+
+        public override void MoveTo(int positionX, int positionY)
         {
             pixels.Clear();
 
@@ -58,18 +36,18 @@ namespace lijohnttle.Media.Photo.Filters
             }
         }
 
-        public bool MoveRight()
+        public override bool MoveRight()
         {
-            if (PositionX >= image.Width - 1)
+            if (PositionX >= Image.Width - 1)
             {
                 return false;
             }
 
-            while (leftPadding >= radius)
+            while (LeftPadding >= Radius)
             {
-                RemoveLeftColumns();
+                RemoveLeftColumn();
 
-                leftPadding--;
+                LeftPadding--;
             }
 
             PositionX++;
@@ -80,45 +58,35 @@ namespace lijohnttle.Media.Photo.Filters
             return true;
         }
 
-        public IEnumerable<IColor> GetPixels() => pixels;
-
-        private void UpdatePaddings()
-        {
-            leftPadding = Math.Min(radius, PositionX);
-            rightPadding = Math.Min(radius, image.Width - 1 - PositionX);
-            topPadding = Math.Min(radius, PositionY);
-            bottomPadding = Math.Min(radius, image.Height - 1 - PositionY);
-        }
-
         private IEnumerable<IColor> IterateColumnByColumn()
         {
             for (int windowX = Left; windowX <= Right; windowX++)
             {
                 for (int windowY = Top; windowY <= Bottom; windowY++)
                 {
-                    yield return image.GetPixel(windowX, windowY);
+                    yield return Image.GetPixel(windowX, windowY);
                 }
             }
         }
 
         private void TryAppendRightColumn()
         {
-            int column = PositionX + radius;
+            int column = PositionX + Radius;
 
-            if (column >= image.Width)
+            if (column >= Image.Width)
             {
                 return;
             }
 
-            for (int row = PositionY - topPadding; row < PositionY + bottomPadding + 1; row++)
+            for (int row = PositionY - TopPadding; row < PositionY + BottomPadding + 1; row++)
             {
-                pixels.Enqueue(image.GetPixel(column, row));
+                pixels.Enqueue(Image.GetPixel(column, row));
             }
 
             return;
         }
 
-        private void RemoveLeftColumns()
+        private void RemoveLeftColumn()
         {
             int kernelHeight = Height;
 
---
+++ src/lijohnttle.Media.Photo.Filters/Tools/SquareKernel.cs
@@ -0,0 +1,79 @@
+﻿using lijohnttle.Media.Photo.Core;
+using System;
+
+namespace lijohnttle.Media.Photo.Filters.Tools
+{
+    public abstract class SquareKernel : Kernel
+    {
+        protected SquareKernel(IImage image, int radius, int positionX, int positionY)
+            : base(image, positionX, positionY)
+        {
+            Radius = radius;
+        }
+
+
+        /// <summary>
+        /// Gets kernel radius.
+        /// </summary>
+        public int Radius { get; }
+
+        /// <summary>
+        /// Gets X coordinate of the left border of the kernel.
+        /// </summary>
+        public int Left => PositionX - LeftPadding;
+
+        /// <summary>
+        /// Gets X coordinate of the right border of the kernel.
+        /// </summary>
+        public int Right => PositionX + RightPadding;
+
+        /// <summary>
+        /// Gets Y coordinate of the top border of the kernel.
+        /// </summary>
+        public int Top => PositionY - TopPadding;
+
+        /// <summary>
+        /// Gets Y coordinate of the bottom border of the kernel.
+        /// </summary>
+        public int Bottom => PositionY + BottomPadding;
+
+        /// <summary>
+        /// Gets width of the kernel.
+        /// </summary>
+        public int Width => LeftPadding + RightPadding + 1;
+
+        /// <summary>
+        /// Gets height of the kernel.
+        /// </summary>
+        public int Height => TopPadding + BottomPadding + 1;
+
+        /// <summary>
+        /// Gets the number of pixels on the left from the position of the kernel.
+        /// </summary>
+        protected int LeftPadding { get; set; }
+
+        /// <summary>
+        /// Gets the number of pixels on the right from the position of the kernel.
+        /// </summary>
+        protected int RightPadding { get; set; }
+
+        /// <summary>
+        /// Gets the number of pixels on the top from the position of the kernel.
+        /// </summary>
+        protected int TopPadding { get; set; }
+
+        /// <summary>
+        /// Gets the number of pixels on the bottom from the position of the kernel.
+        /// </summary>
+        protected int BottomPadding { get; set; }
+
+
+        protected void UpdatePaddings()
+        {
+            LeftPadding = Math.Min(Radius, PositionX);
+            RightPadding = Math.Min(Radius, Image.Width - 1 - PositionX);
+            TopPadding = Math.Min(Radius, PositionY);
+            BottomPadding = Math.Min(Radius, Image.Height - 1 - PositionY);
+        }
+    }
+}
